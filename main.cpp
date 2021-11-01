1155aa76abc5776f98b734c2f727230b2bce6c47
Adrian Drevland <adrian.l.drevland@gmail.com>
2021-11-01 14:38:28 +0100

    djikstra start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -23,6 +24,7 @@ struct Kant {
     Kant(float vekt, Node* tilnode) : m_vekt(vekt), m_tilnode(tilnode) { }
     bool operator > (const Kant& k) const { return m_vekt > k.m_vekt; }
 };
+struct Path;
 struct Graf {
     std::list<Node*> noder;
     Graf() { }
@@ -30,6 +32,7 @@ struct Graf {
     void settinn_node(char navn);
     void settinn_kant(char fra_navn, char til_navn, float vekt);
     float mst();
+    Path Dijkstra(char nFra, char nTil);
 };
 
 Node *Graf::finn_node(char navn) {
@@ -48,10 +51,62 @@ void Graf::settinn_kant(char fra_navn, char til_navn, float vekt) {
     auto fra = finn_node(fra_navn);
     auto til = finn_node(til_navn);
 
-    auto NyKant = new Kant(vekt,til);
-    fra->settinn_kant(*NyKant);
+    if(fra && til){
+        auto NyKant = new Kant(vekt,til);
+        fra->settinn_kant(*NyKant);
+    }
 
 }
+
+struct Path{
+    Path() = default; // empty constructor
+    std::list<Kant> Kanter;
+    float PathCost() const;
+    bool operator > (const Path& rightside)const {return PathCost() > rightside.PathCost();};
+};
+
+float Path::PathCost() const{
+    float Cost{};
+    for(auto Kant: Kanter){
+        Cost += Kant.m_vekt;
+    }
+    return Cost;
+}
+
+
+Path Graf::Dijkstra(char nFra, char nTil){
+
+    std::priority_queue<Path,std::vector<Path>,std::greater<>> Paths;
+    auto S = finn_node(nFra);
+    auto T = finn_node(nTil);
+
+
+    for(auto sKanter: S->m_kanter){
+        Path p{};
+        if(sKanter.m_tilnode != T){
+            p.Kanter.push_back(sKanter);
+            sKanter.m_tilnode->m_besokt = true;
+            if(sKanter.m_tilnode == T) Paths.push(p);
+        }
+        Paths.push(p);
+    }
+
+    while(!Paths.empty()){
+        auto ShortestPath = Paths.top();
+        auto LastKant = ShortestPath.Kanter.back().m_tilnode->m_kanter; //kantene til siste node i nåværende korteste path
+        for(auto sKanter : LastKant){
+            Path p{};
+            if(sKanter.m_tilnode != T && !sKanter.m_tilnode->m_besokt){
+                p.Kanter.push_back(sKanter);
+                sKanter.m_tilnode->m_besokt = true;
+            }
+            Paths.push(p);
+        }
+    }
+
+    return Paths.top();
+}
+
 
 int main() {
     Graf TestGraf;
@@ -73,7 +128,7 @@ int main() {
     TestGraf.settinn_kant('C','E',4);
 
 
-
+   auto ShortestPath = TestGraf.Dijkstra('A','E');
 
 
     return 0;
